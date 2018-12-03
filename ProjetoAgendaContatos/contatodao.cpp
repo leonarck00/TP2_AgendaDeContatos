55cf254dac13754b80314659b2a341af433e3868
leonarck00 <40524700+leonarck00@users.noreply.github.com>
2018-12-02 22:24:07 -0200

    Add files via upload

--- ProjetoAgendaContatos/contatodao.cpp
+++ ProjetoAgendaContatos/contatodao.cpp
@@ -32,8 +32,10 @@ void ContatoDAO::incluir(Contato &objeto) const{
 
 }
 std::list<Contato>* ContatoDAO::listagem()const{
-    std::list<agenda::Contato> *lista = 0;
-    lista = new std::list<agenda::Contato>();
+    //Criando uma Lista com os dados dos contatos
+    //Dados armazenados no arquivo de Contatos
+    std::list<agenda::Contato> *listaContatos = 0;
+    listaContatos = new std::list<agenda::Contato>();
     std::ifstream arquivoAgenda;
     arquivoAgenda.open(nomeDoArquivoNoDisco.toStdString().c_str());
     if(!arquivoAgenda.is_open()){
@@ -44,11 +46,47 @@ std::list<Contato>* ContatoDAO::listagem()const{
     while(!arquivoAgenda.eof()){
         agenda::Contato objeto;
         objeto.materializar(QString::fromStdString(linha));
-        lista->push_back(objeto);
+        listaContatos->push_back(objeto);
         std::getline(arquivoAgenda,linha);
     }
     arquivoAgenda.close();
-    return lista;
+    // Finalizando o processo de criar a lista de contatos
+
+    //Criando uma lista com todos os telefones armazenados
+    //No arquivo de telefones
+    std::list<agenda::Telefone> *listaTelefones = 0;
+    listaTelefones = new std::list<agenda::Telefone>();
+    std::ifstream arquivoTelefones;
+    arquivoTelefones.open("./TelefonesDosContatos.txt");
+    if(!arquivoTelefones.is_open()){
+        throw QString("Arquivo de Agenda nao foi aberto");
+    }
+    std::string linhaTelefone;
+    std::getline(arquivoTelefones,linhaTelefone);
+    while(!arquivoTelefones.eof()){
+        agenda::Telefone fone;
+        fone.materializar(QString::fromStdString(linhaTelefone));
+        listaTelefones->push_back(fone);
+        std::getline(arquivoTelefones,linhaTelefone);
+    }
+    arquivoTelefones.close();
+
+    std::list<agenda::Contato> *listaContatosFinal = 0;
+    listaContatosFinal = new std::list<agenda::Contato>();
+
+    std::list<agenda::Contato>::iterator iListaContatos;
+    for(iListaContatos = listaContatos->begin();iListaContatos!=listaContatos->end();iListaContatos++){
+        agenda::Contato objContato = *iListaContatos;
+        std::list<agenda::Telefone>::iterator iListaTelefones;
+        int idContato = (*iListaContatos).getId();
+        for(iListaTelefones = listaTelefones->begin();iListaTelefones!=listaTelefones->end();iListaTelefones++){
+            if(idContato == (*iListaTelefones).getID() ){
+                objContato.setTelefone((*iListaTelefones));
+            }
+        }
+        listaContatosFinal->push_back(objContato);
+    }
+    return listaContatosFinal;
 }
 
 
